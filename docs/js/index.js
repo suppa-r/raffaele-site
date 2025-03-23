3780609ee2187757427c2302913aedcce64aa78f
suppa-r <supparaffaele43@gmail.com>
2025-03-23 01:28:03 -0400

    refactor: update sidebar navigation styles and improve image responsiveness

--- docs/js/index.js
+++ docs/js/index.js
@@ -86,7 +86,6 @@ function closeMenu() {
 /* -----------------------------------------
   Handle submenu
  ---------------------------------------- */
-const intromain = document.querySelector(".intro-main");
 const btn = document.querySelector(".dropdown-btn");
 const ul = document.querySelector(".sub-menu");
 
@@ -96,14 +95,11 @@ btn.addEventListener("click", () => {
 });
 
 function showSubMenu() {
-  intromain.style.display = "none"; // Hide main content
   ul.style.display = "block"; // Show submenu
   refreshSubMenu(); // Refresh the submenu
 }
 
 function hideSubMenu() {
-  intromain.style.display = "block"; // Show main content
-  intromain.style.display = "flex";
   ul.style.display = "none"; // Hide submenu
 }
 
