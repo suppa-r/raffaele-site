7d8fb024ee6729f7ab6de8a3c4cc969a4b56738c
suppa-r <supparaffaele43@gmail.com>
2025-04-08 18:11:47 -0400

    refactor: implement responsive navigation menu and enhance intro section layout

---
+++ nav bar for intro-1.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector(".bars");
+const intro = document.querySelector(".intro");
+let menustate = false;
+menu.addEventListener("click", showMenu);
+function showMenu() {
+  if (!menustate) {
+    menu.classList.add("is-active");
+    document.querySelector(".nav-links").classList.add("open");
+    menustate = true;
+    intro.style.display = "none";
+  } else {
+    menu.classList.remove("is-active");
+    document.querySelector(".nav-links").classList.remove("open");
+    menustate = false;
+    intro.style.display = "block";
+  }
+}
