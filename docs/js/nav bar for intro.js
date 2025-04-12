46e60824c4c91e4e0af4ab99590a711996bf03a4
suppa-r <supparaffaele43@gmail.com>
2025-04-11 23:26:43 -0400

    feat: Implement responsive navigation bar with toggle functionality
    - Added CSS for a responsive navigation bar with styles for desktop and mobile views.
    - Created JavaScript functionality to toggle the mobile menu and handle menu interactions.
    - Developed HTML structure for the navigation bar and integrated it into the main layout.
    - Included overlay effect for better user experience when the menu is open.
    - Ensured accessibility features such as closing the menu with the escape key and clicking outside the menu.

--- docs/js/nav bar for intro.js
+++ docs/js/nav bar for intro.js
@@ -1,19 +1,20 @@
 const menu = document.querySelector(".bars");
-const intro = document.querySelector(".section-content");
+const intro = document.querySelector(".intro-content");
+const nav = document.querySelector(".nav-links");
 let menustate = false;
-
 menu.addEventListener("click", showMenu);
-
 function showMenu() {
   if (!menustate) {
     menu.classList.add("is-active");
     document.querySelector(".nav-links").classList.add("open");
-    intro.style.display = "none"; // Hide .intro
     menustate = true;
+    intro.style.display = "none";
+    nav.style.display = "block";
   } else {
     menu.classList.remove("is-active");
     document.querySelector(".nav-links").classList.remove("open");
-    intro.style.display = "block"; // Show .intro
     menustate = false;
+    intro.style.display = "block";
+    nav.style.display = "none";
   }
 }
