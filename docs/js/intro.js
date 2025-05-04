4850a0dd158be42acfa30961cfffccddb3de7c5b
suppa-r <supparaffaele43@gmail.com>
2025-05-03 23:25:27 -0400

    Refactor: Remove unused CSS and JavaScript files, update HTML structure and content for improved clarity and performance
    - Deleted intro.css and nav bar for intro.css as they were no longer needed.
    - Removed intro.js and nav bar for intro.js to streamline functionality.
    - Updated intro.html to simplify navigation and enhance user experience.
    - Changed header content from h2 to p for better semantic structure.
    - Added SVG icons for navigation links to improve visual appeal.
    - Revised introductory text to better reflect personal branding and messaging.

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -1,14 +1,20 @@
 const menu = document.querySelector('.bars');
+const intro = document.querySelector('.intro');
+const navLinks = document.querySelector('.nav-links');
 let menustate = false;
+
 menu.addEventListener('click', showMenu);
-function showMenu(){
-    if(!menustate) {
-        menu.classList.add('is-active');
-        document.querySelector('.nav-links').classList.add('open');
-        menustate = true;
+
+function showMenu() {
+    if (!menustate) {
+        menu.classList.add('is-active'); // Add active class to menu
+        navLinks.classList.add('open'); // Show nav links
+        intro.classList.add('hidden'); // Hide .intro
+        menustate = true; // Update state
     } else {
-        menu.classList.remove('is-active');
-        document.querySelector('.nav-links').classList.remove('open');
-        menustate = false;
+        menu.classList.remove('is-active'); // Remove active class from menu
+        navLinks.classList.remove('open'); // Hide nav links
+        intro.classList.remove('hidden'); // Show .intro
+        menustate = false; // Update state
     }
 }
