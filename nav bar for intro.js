cac528ea3213b4c85f9745a354ece63b21ce74ba
suppa-r <supparaffaele43@gmail.com>
2025-05-05 00:35:43 -0400

    Refactor navigation: create new navbar structure and styles; update intro and related files for improved functionality and design.

---
+++ nav bar for intro.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.bars');
+const intro = document.querySelector('.intro');
+let menustate = false;
+
+menu.addEventListener('click', showMenu);
+
+function showMenu() {
+    if (!menustate) {
+        menu.classList.add('is-active');
+        document.querySelector('.nav-links').classList.add('open');
+        intro.style.display = 'none'; // Correctly hide .intro
+        menustate = true;
+    } else {
+        menu.classList.remove('is-active');
+        document.querySelector('.nav-links').classList.remove('open');
+        intro.style.display = 'block'; // Correctly show .intro
+        menustate = false;
+    }
+}
