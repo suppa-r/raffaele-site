d23a1eda717e878f8fc71c88c5343f8e089243e4
suppa-r <supparaffaele43@gmail.com>
2025-05-06 23:00:21 -0400

    Refactor header content: replace h2 with p for improved semantics and update CSS for better layout and responsiveness

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -1,14 +1,26 @@
 const menu = document.querySelector('.bars');
+const intro = document.querySelector('.intro');
 let menustate = false;
+
 menu.addEventListener('click', showMenu);
-function showMenu(){
-    if(!menustate) {
+
+function showMenu() {
+    if (!menustate) {
         menu.classList.add('is-active');
         document.querySelector('.nav-links').classList.add('open');
+        
+        // Hide .intro
+        intro.style.display = 'none';
+        intro.style.visibility = 'hidden';
         menustate = true;
     } else {
         menu.classList.remove('is-active');
         document.querySelector('.nav-links').classList.remove('open');
+        
+        // Show .intro
+        intro.style.display = 'block';
+        intro.style.visibility = 'visible';
+        intro.style.opacity = '1';
         menustate = false;
     }
 }
