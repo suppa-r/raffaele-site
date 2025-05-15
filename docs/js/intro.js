ef615890ff7480e6c1bf479351a54c1358538b19
suppa-r <supparaffaele43@gmail.com>
2025-05-14 21:33:34 -0400

    Remove unused JavaScript files, HTML pages, and update navigation structure for improved sidebar functionality.

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -1,24 +1,10 @@
-const logo = document.querySelector('.logo');
-const intro = document.querySelector('.intro');
-const navlinks = document.querySelector('.nav-links');
-const bars = document.querySelector('.bars');
 
-
-let introState = true;
-
-
-
-bars.addEventListener('click', () => {
-    if (introState) {
-        logo.display = 'block';
-        intro.display = 'none';
-        navlinks.display = 'block';
-        introState = false;
-    } else {
-        logo.display = 'none';
-        intro.display = 'block';
-        navlinks.display = 'none';
-        introState = true;
+    function showSidebar() {
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'flex'
     }
-});
+    function hideSidebar() {
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+    }
 
