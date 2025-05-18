73f11bab0ffc2b23558e140a69ffc86d6082180f
suppa-r <supparaffaele43@gmail.com>
2025-05-18 14:41:06 -0400

    Refactor CSS and JavaScript files for improved layout, responsiveness, and functionality; update HTML structure for better organization and accessibility

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -1,10 +1,12 @@
+  const logo = document.querySelector('.logo')
 
-    function showSidebar() {
-      const sidebar = document.querySelector('.sidebar')
-      sidebar.style.display = 'flex'
-    }
-    function hideSidebar() {
-      const sidebar = document.querySelector('.sidebar')
-      sidebar.style.display = 'none'
-    }
-
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+  logo.style.display = 'none'
+  }
+  function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+    logo.style.display = 'block'
+  }
