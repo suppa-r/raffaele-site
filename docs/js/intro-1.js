f9dc9c6dc06e950952de437b866aab43efc6a1eb
suppa-r <supparaffaele43@gmail.com>
2025-05-21 15:43:26 -0400

    Refactor CSS for dot indicators and crew details layout; update SVG icon in HTML for improved design consistency

--- docs/js/intro-1.js
+++ docs/js/intro-1.js
@@ -2,12 +2,14 @@ const logo = document.querySelector('.logo');
 const crewdetails = document.querySelectorAll('.crew-details');
 const sidebar = document.querySelector('.sidebar');
 const menubutton = document.querySelector('.menu-button');
+const dotindictors = document.querySelector('.dot-indicators');
 
 function showSidebar() {
   sidebar.style.display = 'flex';
   logo.style.display = 'none';
   crewdetails.forEach(el => el.style.display = 'none');
   menubutton.style.display = 'none';
+  dotindictors.style.display = 'none';
 }
 
 function hideSidebar() {
@@ -15,4 +17,6 @@ function hideSidebar() {
   logo.style.display = 'block';
   crewdetails.forEach(el => el.style.display = 'block');
   menubutton.style.display = 'block';
-}
+  dotindictors.style.display = 'block';
+ }
+
