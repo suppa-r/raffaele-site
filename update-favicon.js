fc501aeb26fd27f45fda3c1e55a332ea0e56791f
suppa-r <supparaffaele43@gmail.com>
2025-10-14 14:16:32 -0400

    Refactor code structure for improved readability and maintainability

--- update-favicon.js
+++
@@ -1,6 +0,0 @@
-function updateFavicon() {
-    const favicon = document.querySelector('link[rel="icon"]');
-    const path = document.hidden ? '/favicon-inactive.ico' : '/favicon.ico';
-    favicon?.setAttribute('href', path)
-}
-document.addEventListener('visibilitychange', updateFavicon)
